5d4eef2148aff8f95a5637a0886df2b3996cf8e7
rdoerre8 <rdoerre8@tamu.edu>
2022-09-16 11:53:07 -0500

    Book 2 collection assignment

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,10 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Book.create!([
+  {title: "The life of a turtle", author: "Who knows?", price: 3.0, published_date: "2022-09-09"},
+  {title: "The mirror, the lion, and the audacity of the little dog", author: "Who knows?", price: 24.0, published_date: "2022-09-19"},
+  {title: "Origami for cats", author: "Cat Lady", price: 0.0, published_date: "2022-09-09"},
+  {title: "The weight of a hundred", author: "Who knows?", price: 23.0, published_date: "2022-09-08"}
+])
+User.create!([
+  {username: "Turtle Person"},
+  {username: "Axolotl"}
+])
